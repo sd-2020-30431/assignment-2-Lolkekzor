5869a5c1870bc3ad370ff6a6d9d7be49c4ee9e62
lolkekzor <chioreantudoroctavian@gmail.com>
2020-04-22 16:07:28 +0300

    Add admin view for lists

--- project/backend/wasteless/lists/admin.py
+++ project/backend/wasteless/lists/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import List, ListItem
 
-# Register your models here.
+
+class ListItemInline(admin.StackedInline):
+    model = ListItem
+    extra = 3
+
+
+class ListAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'user']})
+    ]
+    inlines = [ListItemInline]
+
+
+admin.site.register(List, ListAdmin)
